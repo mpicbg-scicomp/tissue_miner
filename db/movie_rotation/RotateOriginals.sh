4f5c154e5468aaf7e0df373e9688625a036c12d1
Holger Brandl <brandl@mpi-cbg.de>
2015-07-30 13:35:57 +0200

    made gnu parallel optional

--- db/movie_rotation/RotateOriginals.sh
+++ db/movie_rotation/RotateOriginals.sh
@@ -13,31 +13,32 @@ NB_CPU=$(echo $(grep -c processor /proc/cpuinfo)/2 | bc)
 # half nb cpu on mac
 #NB_CPU=$(echo $(/usr/sbin/system_profiler -detailLevel full SPHardwareDataType | awk '/Total Number of Cores/{print $5}')/2 | bc)
 
-## fixme not generic
-## cluster support
+### fixme not generic
+### cluster support
 export PATH=/projects/project-raphael/parallel-20140422/src:$PATH
-## bioinfo support
+### bioinfo support
 export PATH=/home/brandl/bin/parallel-20140422/src:$PATH
 
 #export trafoFile="$movieDbDir/transformation.txt"
 export trafoFile="$movieDbDir/Segmentation/transformation.txt"
 
-if [ -f "$trafoFile" ]; then
-#    mysub "rotate_images_$(basename $movieDbDir)" '
-    for originalPng in $(find $movieDbDir/Segmentation -name "original.png" | sort); do
-        # DEBUG originalPng=/lustre/projects/project-raphael/movie_dbs/testing/WT_25deg_111103/Segmentation/frame0204/original.png
-
-        trafoPngOutput=${originalPng%%.png}_trafo.png
-
-#        if [ ! -f "$trafoPngOutput" ]; then
-        echo "rotating $originalPng to $trafoPngOutput ..."
-        #sem --no-notice -j6  $TM_HOME/db/movie_rotation/transform_images.sh $originalPng $trafoFile $trafoPngOutput
-        sem -j$NB_CPU  $TM_HOME/db/movie_rotation/transform_images.sh $originalPng $trafoFile $trafoPngOutput
-#        $TM_HOME/db/movie_rotation/transform_images.sh $originalPng $trafoFile $trafoPngOutput
-#		fi
-    done
-#    '  -R span[hosts=1] -n 6 | blockScript
-	sem --wait
-else
-    echo "no transformation.txt, skipping png rotation"
+if [ ! -f "$trafoFile" ]; then
+    echo "no transformation.txt, skipping png rotation"; exit
 fi
+
+for originalPng in $(find $movieDbDir/Segmentation -name "original.png" | sort); do
+    # DEBUG originalPng=/lustre/projects/project-raphael/movie_dbs/testing/WT_25deg_111103/Segmentation/frame0204/original.png
+
+    trafoPngOutput=${originalPng%%.png}_trafo.png
+
+    echo "rotating $originalPng to $trafoPngOutput ..."
+    #sem --no-notice -j6  $TM_HOME/db/movie_rotation/transform_images.sh $originalPng $trafoFile $trafoPngOutput
+
+    if [ -z "$(which sem)" ]; then
+        $TM_HOME/db/movie_rotation/transform_images.sh $originalPng $trafoFile $trafoPngOutput
+    else
+        sem -j$NB_CPU  $TM_HOME/db/movie_rotation/transform_images.sh $originalPng $trafoFile $trafoPngOutput
+    fi
+done
+
+sem --wait
