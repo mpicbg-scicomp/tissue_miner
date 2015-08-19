105673278de4ea4a1877409e766707a46a5afebe
Holger Brandl <holgerbrandl@gmail.com>
2015-08-19 16:31:42 +0200

    removed no longer needed script

--- parser/install_parser.sh
+++
@@ -1,8 +0,0 @@
-cd /projects/project-raphael/scripts/flywing_base/parser/imageParser_150430-01/imageParser
-tar -xvzf imageParser_150430-01.tar.gz
-cd imageParser_150430-01
-
-## change INCDIRS to
-#INCDIRS = -IFramework -IParser -I/usr/include/netcdf/
-
-
