3e7ab82c0e4f75d4ca46576b3239fc3e8056e0a6
Holger Brandl <holgerbrandl@gmail.com>
2015-09-22 12:05:10 +0200

    increased dplyr version check to 0.4.3 and simplified setup

--- Setup.R
+++ Setup.R
@@ -1,13 +1,5 @@
 #!/usr/bin/env Rscript
 
-#devtools::source_url("http://dl.dropbox.com/u/113630701/rlibs/base-commons.R")
-
-#devtools::source_url("https://dl.dropboxusercontent.com/u/113630701/datautils/R/core_commons.R")
-#devtools::source_url("https://dl.dropboxusercontent.com/u/113630701/datautils/R/ggplot_commons.R")
-
-## Missing dependecies
-#install.packages('codetools')
-
 ## preselect a cran mirror
 r = getOption("repos") # hard code the UK repo for CRAN 
 r["CRAN"] = "http://ftp5.gwdg.de/pub/misc/cran/"
@@ -22,12 +14,7 @@ scriptsDir=Sys.getenv("TM_HOME")
 source(file.path(scriptsDir, "commons/TMCommons.R"))
 
 
-#devtools::install_github("hadley/lazyeval")
-#install.packages("Rcpp")
-#install.packages("dplyr")
-
 ## spefic libraries needed in som of the workflows
-
 require.auto(zoo) # for na.locf
 require.auto(sp)
 require.auto(docopt)
@@ -48,10 +35,10 @@ require.auto(rgeos)
 require.auto(hash)
 
 ## force dplyr 0.4.1 since 0.4.2 is buggy
-if(packageVersion("dplyr") != "0.4-1"){
-    packageurl = "http://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_0.4.1.tar.gz"
-    install.packages(packageurl, repos=NULL, type="source", dependencies = TRUE)
-}
+#if(packageVersion("dplyr") != "0.4-1"){
+#    packageurl = "http://cran.r-project.org/src/contrib/Archive/dplyr/dplyr_0.4.1.tar.gz"
+#    install.packages(packageurl, repos=NULL, type="source", dependencies = TRUE)
+#}
 
 
 ########################################################################################################################
@@ -60,10 +47,9 @@ if(packageVersion("dplyr") != "0.4-1"){
 
 
 sessionInfo()
-## Required minimal resuilt to run the workflow
-#> sessionInfo()
-#R version 3.1.2 (2014-10-31)
-#Platform: x86_64-unknown-linux-gnu (64-bit)
+#R version 3.2.2 (2015-08-14)
+#Platform: x86_64-pc-linux-gnu (64-bit)
+#Running under: CentOS release 6.3 (Final)
 #
 #locale:
 # [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8     LC_MONETARY=en_US.UTF-8
@@ -74,10 +60,13 @@ sessionInfo()
 #[1] parallel  grid      stats     graphics  grDevices utils     datasets  methods   base
 #
 #other attached packages:
-# [1] doMC_1.3.3         iterators_1.0.7    foreach_1.4.2      RColorBrewer_1.1-2 png_0.1-7          sqldf_0.4-10       RSQLite_1.0.0
-# [8] DBI_0.3.1          gsubfn_0.6-6       proto_0.3-10       data.table_1.9.4   scales_0.2.4       ggplot2_1.0.1      digest_0.6.8
-#[15] tidyr_0.2.0        magrittr_1.5       dplyr_0.4.1        reshape2_1.4.1     stringr_1.0.0      plyr_1.8.1
+# [1] hash_2.2.6         rgeos_0.3-12       RBGL_1.44.0        igraph_1.0.1       graph_1.46.0       docopt_0.4.3.3     sp_1.2-0
+# [8] zoo_1.7-12         doMC_1.3.3         iterators_1.0.7    foreach_1.4.2      RColorBrewer_1.1-2 png_0.1-7          sqldf_0.4-10
+#[15] RSQLite_1.0.0      DBI_0.3.1          gsubfn_0.6-6       proto_0.3-10       data.table_1.9.6   scales_0.3.0       ggplot2_1.0.1
+#[22] digest_0.6.8       tidyr_0.3.1        magrittr_1.5       dplyr_0.4.3        reshape2_1.4.1     stringr_1.0.0      plyr_1.8.3
+#[29] devtools_1.9.1     codetools_0.2-14
 #
 #loaded via a namespace (and not attached):
-# [1] assertthat_0.1   bitops_1.0-6     chron_2.3-45     codetools_0.2-11 colorspace_1.2-6 devtools_1.7.0   gtable_0.1.2     httr_0.6.1
-# [9] MASS_7.3-39      munsell_0.4.2    Rcpp_0.11.5      RCurl_1.95-4.5   stringi_0.4-1    tools_3.1.2
+# [1] Rcpp_0.12.1         tools_3.2.2         memoise_0.2.1       gtable_0.1.2        lattice_0.20-33     curl_0.9.3          httr_1.0.0
+# [8] stats4_3.2.2        R6_2.1.1            BiocGenerics_0.14.0 MASS_7.3-43         assertthat_0.1      colorspace_1.2-6    stringi_0.5-5
+#[15] munsell_0.4.2       chron_2.3-47
--- commons/TMCommons.R
+++ commons/TMCommons.R
@@ -44,8 +44,8 @@ check_version = function(pkg_name, min_version) {
                found %s, need at least %s", pkg_name, cur_version, min_version))
 }
 
-check_version("dplyr", "0.4-1")
-check_version("magrittr", "1.5")
+check_version("dplyr", "0.4-3")
+#check_version("magrittr", "1.5")
 
 ####################################################################################################
 ## Helper functions
