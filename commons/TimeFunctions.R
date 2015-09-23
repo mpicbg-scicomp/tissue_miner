fd78766a93ec87dc14cace79e450f6ee5ba05652
Raphael Etournay <etournay@mpi-cbg.de>
2015-09-23 17:00:15 +0200

    fixed chunk_time_into_intervals and synchronize_frames functions

--- commons/TimeFunctions.R
+++ commons/TimeFunctions.R
@@ -1,11 +1,11 @@
 
-## align_movie_start: apply a time offset such that the counting starts at the min common time point of the selected movies ####
+## align_movie_start() ####
 calcRefTime <- function(movies){ get_movie_time_shift(movies) %$% max(time_shift) }
 align_movie_start <- function(movieData, moviesDirs){
   
-  # Description: count number of cells per frame, in ROIs
-  # Usage: in combination with multi_db_query(), ex: multi_db_query(movieDirs, mqf_cell_count, selectedRois)
-  # Arguments: movieDb = opened DB connection,  movieDbDir = path to a given movie folder
+  # Description: : apply a time offset such that the counting starts at the min common time point of the selected movies
+  # Usage: align_movie_start(movieData, moviesDirs)
+  # Arguments: movieData = input dataframe,  moviesDirs = list of paths to movie folders
   
   movies <- ac(unique(movieData$movie))
   refTime <- calcRefTime(movies)
@@ -29,7 +29,6 @@ align_movie_start <- function(movieData, moviesDirs){
   
   return(mdCumSumFilt)
 }
-
 ## chunk_time_into_intervals() ####
 chunk_time_into_intervals <- function(df, deltaT){
   
@@ -40,6 +39,7 @@ chunk_time_into_intervals <- function(df, deltaT){
   # Make sure that the input data contains the time definition in seconds
   if (! "time_sec" %in% names(df)| ! "time_shift" %in% names(df)){stop("In addTimeIntFunc, time_sec and/or time_shift column not present in input dataframe")}
   
+  # Use the developmental time for synchronization
   df %<>% mutate(time_sec=time_sec+time_shift)
   
   # Find upper time limit
@@ -57,54 +57,43 @@ chunk_time_into_intervals <- function(df, deltaT){
  
   return(dataWithIntFilt) 
 }
-
-## sync_time_into_intervals() ####
-synchronize_frames <- function(df, deltaT){
+## synchronize_frames() ####
+synchronize_frames <- function(df, deltaT, trimTails=T){
   
   # Description: chunk the time into synchronized intervals of deltaT in seconds
-  # Usage: sync_time_into_intervals(df, deltaT)
-  # Arguments: df = input dataframe,  deltaT = time interval length in seconds
+  # Usage: synchronize_frames(df, deltaT,trimTails) where trimTails is optional
+  # Arguments: df = input dataframe, 
+  #            deltaT = time interval length in seconds
+  #            trimTails = if TRUE (default), remove non-overlapping time data
   
-  # Make sure that the input data contains the time definition in seconds
-  if (! "time_sec" %in% names(df)| ! "time_shift" %in% names(df)){stop("In addTimeIntFunc, time_sec and/or time_shift column not present in input dataframe")}
+  chunkedData <- chunk_time_into_intervals(df, deltaT)
   
-  # Find upper time limit
-  tmax <- max(df$time_sec)
+  # Summarize dev_time and optimal frame for each time interval
+  syncFrames <- chunkedData %>%
+    group_by(movie,interval_mid) %>% 
+    summarise(syncFrame=round(mean(frame)),
+              dev_time=mean(dev_time)) %>% print_head()
   
+  # Remove non overlapping data in time
+  if (trimTails) {
+    syncFrames %<>% ungroup() %>%
+      group_by(movie) %>%
+      mutate(max_interval=max(interval_mid),
+             min_interval=min(interval_mid)) %>%
+      ungroup() %>%
+      filter(interval_mid<=min(max_interval) & interval_mid>=max(min_interval)) %>%
+      print_head()
+  }
+  
+  return(syncFrames)
 }
 
-## DEBUG sync_time_into_intervals() ####
-movieDirs <- file.path(movieDbBaseDir, c("WT_25deg_111102","WT_25deg_111103"))
-selectedRois<- c("blade")
+## DEBUG synchronize_frames() ####
+if (F){
+  movieDirs <- file.path(movieDbBaseDir, c("WT_25deg_111102","WT_25deg_111103","WT_25deg_120531"))
+  syncFrames <- multi_db_query(movieDirs, mqf_dev_time) %>%
+    synchronize_frames(3600) %>% print_head()
+}
+ 
 
-# Get frame and time from DB and alignement model (time_sec are aligned)
-timeRaw <- multi_db_query(movieDirs, mqf_cell_count, selectedRois) %>%
-  mutate(time_sec=time_sec+time_shift) %>%
-  select(-c(timeInt_sec,time_shift,Freq)) %>% print_head()
 
-df <- sync_time_into_intervals(timeRaw, 300)
-
-# Split total max time into intervals of deltaTsec
-deltaTsec <- 300 # 5min
-tmax=max(timeRaw$time_sec)
-timeIntervals <- data.frame(interval_start=seq(1,tmax, deltaTsec)[1:(length(seq(1,tmax, deltaTsec))-1)],
-                            interval_end=seq(0,tmax, deltaTsec)[2:length(seq(1,tmax, deltaTsec))]) %>%
-  mutate(interval_mid=round(0.5*(interval_start+interval_end))) %>% print_head()
-
-# Find optimum frame for each interval
-dataWithInt <- timeRaw %>%
-  # Assign all intervals to each frame of the data set
-  merge(timeIntervals, by=c(), all=T) %>%
-  # Map each frame/timeAPF to a unique interval
-  filter(time_sec >= interval_start & time_sec <=interval_end) %>%
-  # Summarize time and frame for each interval
-  group_by(movie,roi,interval_mid) %>%
-  summarise(frame.avg=round(mean(frame)),
-            timeAPF.avg=mean(timeAPF)) %>% 
-  ungroup() %>%
-  group_by(movie) %>%
-  mutate(max_interval=max(interval_mid),
-         min_interval=min(interval_mid)) %>%
-  ungroup() %>%
-  filter(interval_mid<=min(max_interval) & interval_mid>=max(min_interval)) %>%
-  print_head()
