d8606e59b31c6aa610c4caa1a98830384bb4baea
Raphael Etournay <etournay@mpi-cbg.de>
2015-10-07 16:18:07 +0200

    fixed a bug in mqf_fg_nematics_cell_elong

--- commons/BaseQueryFunctions.R
+++ commons/BaseQueryFunctions.R
@@ -279,7 +279,7 @@ mqf_fg_nematics_cell_elong <- function(movieDir, rois=c(), cellContour=F, displa
   # Output: a dataframe
   
   movieDb <- openMovieDb(movieDir)
-  
+
   # Send a SQL query to get the cell elongation tensor in each frame
   queryResult <- dbGetQuery(movieDb,
                             "select cell_id, frame, center_x, center_y, elong_xx, elong_xy 
@@ -293,8 +293,6 @@ mqf_fg_nematics_cell_elong <- function(movieDir, rois=c(), cellContour=F, displa
            y1=center_y-0.5*displayFactor*norm*sin(phi),
            x2=center_x+0.5*displayFactor*norm*cos(phi),
            y2=center_y+0.5*displayFactor*norm*sin(phi)) %>%
-    # remove unecessary columns
-    select(-c(displayFactor)) %>%
     addTimeFunc(movieDb, .) %>% 
     mutate(movie=basename(movieDir)) %>% add_dev_time()
   
