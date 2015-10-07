dd161a3dd33e534fababa92ccc5178d35dd86432
Raphael Etournay <etournay@mpi-cbg.de>
2015-10-07 17:25:03 +0200

    cleaned up old stuff and added CPU number automatic detection

--- db/movie_rotation/RotateOriginals.sh
+++ db/movie_rotation/RotateOriginals.sh
@@ -4,30 +4,25 @@ if [ $# -ne 1 ]; then echo "Usage: `basename $0` <movie db directory>"; exit; fi
 
 # http://stackoverflow.com/questions/3915040/bash-fish-command-to-print-absolute-path-to-a-file
 export movieDbDir=$(readlink -e $1)
-    #export movieDbDir=$(pwd)
 
-# Half nb cpu on Linux machines
-NB_CPU=$(echo $(grep -c processor /proc/cpuinfo)/2 | bc)
-#NB_CPU="4"
+# Define number of CPUs on Linux or MacOSX OS
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   NB_CPU=$(echo $(grep -c processor /proc/cpuinfo)/2 | bc)
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   NB_CPU=$(echo $(/usr/sbin/system_profiler -detailLevel full SPHardwareDataType | awk '/Total Number of Cores/{print $5}')/2 | bc)
+fi
 
-# half nb cpu on mac
-#NB_CPU=$(echo $(/usr/sbin/system_profiler -detailLevel full SPHardwareDataType | awk '/Total Number of Cores/{print $5}')/2 | bc)
-
-### fixme not generic
-### cluster support
-export PATH=/projects/project-raphael/parallel-20140422/src:$PATH
-### bioinfo support
-export PATH=/home/brandl/bin/parallel-20140422/src:$PATH
-
-#export trafoFile="$movieDbDir/transformation.txt"
+# Define path to transformation.txt file and test if it exists
 export trafoFile="$movieDbDir/Segmentation/transformation.txt"
 
 if [ ! -f "$trafoFile" ]; then
     echo "no transformation.txt, skipping png rotation"; exit
 fi
 
+
+# Rotate and/or flip-flop images using ImageMagick
 for originalPng in $(find $movieDbDir/Segmentation -name "original.png" | sort); do
-    # DEBUG originalPng=/lustre/projects/project-raphael/movie_dbs/testing/WT_25deg_111103/Segmentation/frame0204/original.png
 
     trafoPngOutput=${originalPng%%.png}_trafo.png
 
