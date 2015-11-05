610b209b35ab68cd73782d08cb45bd5f74236529
Holger Brandl <holgerbrandl@gmail.com>
2015-11-05 14:51:29 +0100

    better separation of api and test code

--- misc/scala/src/main/scala/de/mpicbg/eaton/tissueminer/TraversalUtils.scala
+++ misc/scala/src/main/scala/de/mpicbg/eaton/tissueminer/TraversalUtils.scala
@@ -1,15 +1,5 @@
 package de.mpicbg.eaton.tissueminer
 
-import Generations
-import de.mpicbg.eaton.tissueminer.Tables
-import de.mpicbg.eaton.tissueminer.Tables._
-import slick.driver.SQLiteDriver.api._
-import slick.lifted.TableQuery
-
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration._
-import de.mpicbg.eaton.tissueminer.TraversalUtils._
-
 
 /**
   * Implicit reference getters for cells and bonds
@@ -39,7 +29,6 @@ object TraversalUtils {
 
     def mother(implicit tissueHist: Seq[Tables.CellHistoriesRow]): Option[Tables.CellHistoriesRow] = {
       tissueHist.find(someCell => {
-        //        val children = List(someCell.leftDaughterCellId.getOrElse(-1), someCell.leftDaughterCellId.getOrElse(-1))
         val children = List(someCell.leftDaughterCellId.getOrElse(-1), someCell.leftDaughterCellId.getOrElse(-1))
         children.contains(cell.cellId)
       })
@@ -48,23 +37,4 @@ object TraversalUtils {
 }
 
 
-package object TraversalTest {
 
-
-  val db = Database.forURL("jdbc:sqlite:/Users/brandl/Desktop/demo_ForTissueMiner.sqlite", driver = "org.sqlite.JDBC")
-
-  //  http://slick.typesafe.com/doc/3.1.0-RC3/gettingstarted.html#database-configuration
-  //  db.createSession()
-
-  val cellHistories = TableQuery[CellHistories]
-
-  implicit val cells: Seq[Tables.CellHistoriesRow] = Await.result(db.run(cellHistories.result), Inf)
-
-
-  val id = 10012
-  val query = CellHistories.filter(_.cellId === id)
-  val cellInfo = Await.result(db.run(query.result), Inf).head
-
-  val daughter = cellInfo.leftDaughter
-  val mother = cellInfo.mother
-}
--- misc/scala/src/main/scala/de/mpicbg/eaton/tissueminer/misc/TraversalTest.scala
+++ misc/scala/src/main/scala/de/mpicbg/eaton/tissueminer/misc/TraversalTest.scala
@@ -1,7 +1,7 @@
-package de.mpicbg.eaton.tissueminer
+package de.mpicbg.eaton.tissueminer.misc
 
 import de.mpicbg.eaton.tissueminer.Tables
-import de.mpicbg.eaton.tissueminer.Tables.CellHistories
+import de.mpicbg.eaton.tissueminer.Tables._
 import de.mpicbg.eaton.tissueminer.TraversalUtils._
 import slick.driver.SQLiteDriver.api._
 import slick.lifted.TableQuery
@@ -9,27 +9,29 @@ import slick.lifted.TableQuery
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration._
 
+
 /**
-  * Get the roots of the lineage trees
+  * Document me!
   *
   * @author Holger Brandl
   */
-object LineageRoots extends App {
+object TraversalTest {
+
 
   val db = Database.forURL("jdbc:sqlite:/Users/brandl/Desktop/demo_ForTissueMiner.sqlite", driver = "org.sqlite.JDBC")
+
+  //  http://slick.typesafe.com/doc/3.1.0-RC3/gettingstarted.html#database-configuration
+  //  db.createSession()
+
   val cellHistories = TableQuery[CellHistories]
 
   implicit val cells: Seq[Tables.CellHistoriesRow] = Await.result(db.run(cellHistories.result), Inf)
 
 
-  // define a recursive function to trace back the lineage
-  def findLineageRoot(cell: Tables.CellHistoriesRow): Tables.CellHistoriesRow = {
-    cell.mother match {
-      case Some(motherCell) => findLineageRoot(motherCell)
-      case None => cell
-    }
-  }
+  val id = 10012
+  val query = CellHistories.filter(_.cellId === id)
+  val cellInfo = Await.result(db.run(query.result), Inf).head
 
-
-  private val treeRoots = cells.map(findLineageRoot).distinct
+  val daughter = cellInfo.leftDaughter
+  val mother = cellInfo.mother
 }
