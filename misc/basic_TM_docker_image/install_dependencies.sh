2562d174dfe473463078e7ef6c0b2ff16297bdc6
Raphael Etournay <etournay@mpi-cbg.de>
2016-03-11 23:35:54 +0100

    just moved here from parent directory

---
+++ misc/basic_TM_docker_image/install_dependencies.sh
@@ -0,0 +1,78 @@
+########################################################################################################################
+## Install requirements for TissueMiner
+
+## (optionally) install R 3.2.1 (see http://www.thertrader.com/2014/09/22/installing-rrstudio-on-ubuntu-14-04/)
+if [ -z "$(which R)" ]; then
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list
+#sudo  cat /etc/apt/sources.list
+
+# work around permission issues
+# see http://stackoverflow.com/questions/10255082/installing-r-from-cran-ubuntu-repository-no-public-key-error
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+sudo apt-get update
+sudo apt-get install -y r-base
+fi
+
+## enforce at least R 3.2
+if [ -z "$(R --version | grep '3.2')" ]; then
+    echo "R is too old. At least v3.2 is required to run TissueMiner" >&2
+    exit 1
+fi
+
+## install snakemake
+sudo apt-get install -y python3-setuptools
+easy_install3 snakemake
+
+## install curl
+sudo apt-get install -y libcurl4-gnutls-dev
+
+## install lixbml
+sudo apt-get install -y libxml2-dev
+
+## install imagemagick needed for image rotation
+sudo apt-get install -y bc imagemagick
+
+## install openssl for git
+sudo apt-get install -y libssl-dev
+
+## install rgeos needed for polygon operations
+sudo apt-get install -y libgeos-dev
+
+## install sqlite
+sudo apt-get install -y sqlite3 libsqlite3-dev
+
+## install avconv needed movie rendering
+sudo apt-get install -y libav-tools
+
+## install graphviz needed for snakemake graph rendering
+sudo apt-get install -y graphviz
+
+
+## sem is disabled for now, since it's not essential to have it.
+## sem for image conversion (see http://askubuntu.com/questions/12764/where-do-i-get-a-package-for-gnu-parallel)
+#sudo apt-get install -y parallel
+#sudo apt-get install -y wget
+#wget http://ftp.gnu.org/gnu/parallel/parallel-20140422.tar.bz2
+#tar -xvjf parallel*
+#cd parallel*
+##less README
+#./configure
+#make clean
+#make
+
+
+#sudo make install
+#./src/sem --bibtex
+# note added to PATH via .bash_profile in docker image
+
+#### install image parser dependencies
+
+# netcds http://askubuntu.com/questions/79418/installing-netcdf
+#sudo apt-get install -y libnetcdf-dev
+# see http://askubuntu.com/questions/382444/qt-headers-and-libraries-not-found
+sudo apt-get install -y qt4-dev-tools libqt4-core
+#sudo apt-get install libqt4-dev libqt4-opengl-dev libqtwebkit-dev qt4-linguist-tools qt4-qmake
+
+#
+### git (already present when building docker file but is required for direct ubuntu installation)
+sudo apt-get install -y git
