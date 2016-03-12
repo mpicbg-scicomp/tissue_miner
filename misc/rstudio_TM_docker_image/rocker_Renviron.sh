0690ef644a8cd8f98f2238fdb1eed7e618202ede
Raphael Etournay <etournay@mpi-cbg.de>
2016-03-12 01:25:53 +0100

    modified Rstudio config

---
+++ misc/rstudio_TM_docker_image/rocker_Renviron.sh
@@ -0,0 +1,5 @@
+## Define where the TissueMiner directory is located
+TM_HOME="/tissue_miner/"
+
+
+
--- misc/rstudio_TM_docker_image/rocker_bash_profile.sh
+++
@@ -1,7 +0,0 @@
-## Define where the TissueMiner directory is located
-export TM_HOME="/tissue_miner/"
-
-export PATH=$TM_HOME/db:$TM_HOME/shear:$TM_HOME/roi:$TM_HOME/misc:$TM_HOME/movies:$TM_HOME/shear_contributions:$TM_HOME/topology:$TM_HOME/triangles:$TM_HOME/lineage:$PATH
-
-
-
