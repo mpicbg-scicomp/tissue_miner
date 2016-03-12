ebf6a68a427bbe8ff2dc2ecb846ce53964c9e10f
Raphael Etournay <etournay@mpi-cbg.de>
2016-03-12 13:49:53 +0100

    setup Rmarkdown

---
+++ misc/rstudio_TM_docker_image/RstudioSetup.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+## preselect a cran mirror
+r = getOption("repos") # hard code the UK repo for CRAN
+
+r["CRAN"] = "http://ftp5.gwdg.de/pub/misc/cran/"
+options(repos = r)
+rm(r)
+
+# Install packages requierd to run the TM Manual as a markdown document
+install.packages("knitr")
+install.packages("htmltools")
+install.packages("caTools")
+install.packages("bitops")
+install.packages("rmarkdown")
