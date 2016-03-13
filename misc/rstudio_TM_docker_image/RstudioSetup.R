06c07b12c6c755c9fc4bce908b359b2d184dfcf6
Raphael Etournay <etournay@mpi-cbg.de>
2016-03-13 18:26:59 +0100

    added a note to make ggsave2 compliant with svg again

--- misc/rstudio_TM_docker_image/RstudioSetup.R
+++ misc/rstudio_TM_docker_image/RstudioSetup.R
@@ -7,9 +7,13 @@ r["CRAN"] = "http://ftp5.gwdg.de/pub/misc/cran/"
 options(repos = r)
 rm(r)
 
-# Install packages requierd to run the TM Manual as a markdown document
+# Install packages required to run the TM Manual as a markdown document
 install.packages("knitr")
 install.packages("htmltools")
 install.packages("caTools")
 install.packages("bitops")
 install.packages("rmarkdown")
+
+# Install packages required to save graphs as svg
+
+#install.packages("svglite")
