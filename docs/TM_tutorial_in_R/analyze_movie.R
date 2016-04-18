03d586266fabe7879a0d8b990ebf38387982fd1e
Raphael Etournay <etournay@mpi-cbg.de>
2016-04-18 14:42:26 +0200

    initial commit

---
+++ docs/TM_tutorial_in_R/analyze_movie.R
@@ -0,0 +1,96 @@
+#!/usr/bin/env Rscript
+
+## Setup  I/O ####
+argv = commandArgs(TRUE)
+
+if((length(argv) < 2) | (length(argv) > 3)){
+  stop("Usage: analyze_movie.R <movie_db_directory> <output_directory> <'ROI list in quotes'>")
+}else{
+  movieDir=normalizePath(argv[1])
+  if(is.na(file.info(movieDir)$isdir)) stop(paste("movie directory does not exist"))
+  print(movieDir)
+  
+  outDir=normalizePath(argv[2])
+  dir.create(outDir)
+  setwd(outDir)
+  print(outDir)
+  
+  if(is.na(argv[3])) ROIlist=c("raw") else{
+    library(stringr)
+    ROIlist=unlist(str_split(argv[3], "( *, *| *; *)| +"))
+    if (ROIlist[1]=="") ROIlist=c("raw")}
+  print(ROIlist)
+}
+
+
+scriptsDir=Sys.getenv("TM_HOME")
+if(is.na(file.info(scriptsDir)$isdir)){
+  stop(paste("TM_HOME not correctly defined (",scriptsDir ,")"))
+}
+
+# Load TissueMiner libraries
+source(file.path(scriptsDir, "commons/TMCommons.R"))
+source(file.path(scriptsDir, "commons/BaseQueryFunctions.R"))
+source(file.path(scriptsDir, "commons/TimeFunctions.R"))
+source(file.path(scriptsDir, "config/flywing_tm_config.R"))
+
+db <- openMovieDb(movieDir)
+
+## Cell Area ####
+cellArea <- mqf_fg_cell_area(movieDir, rois = ROIlist, cellContour = T)
+
+print("Creating cell_area_pattern.mp4...")
+
+l_ply(ROIlist, function(current_roi){
+  cellArea %>% filter(roi == current_roi) %>%
+    render_movie(paste0("cell_area_pattern_",current_roi,".mp4"), list(
+      geom_polygon(aes(x_pos, y_pos, group=cell_id, fill=area)),
+      scale_fill_gradientn(name="area (px)",
+                           colours=c("black", "blue", "green", "yellow", "red"),
+                           limits=c(0,quantile(cellArea$area, probs = 99.9/100)),
+                           na.value = "red")
+    ))
+}, .inform = T)
+
+print("CellAreaPattern.mp4 done !"); print("")
+
+
+print("Save plot: averaged_cell_area.pdf")
+ggsave2(mqf_cg_roi_cell_area(movieDir, rois = ROIlist) %>% 
+          ggplot(aes(dev_time, area.avg, color=movie)) +
+          geom_line() +
+          xlab("Time [h]") +
+          ylab("<Cell area> [px]")  +
+          facet_wrap(~roi) +
+          ggtitle("averaged_cell_area"), outputFormat = "pdf")
+
+print("")
+print("Save plot: cell_area_distribution.pdf")
+ggsave2(mqf_fg_cell_area(movieDir, rois = ROIlist) %>% 
+          ggplot(aes(area, fill=movie)) +
+          geom_histogram(color="white") +
+          xlab("Cell area [px]")  +
+          facet_wrap(~roi) +
+          ggtitle("cell_area_distribution"), outputFormat = "pdf")
+
+print("")
+print("Save plot: cell_area_boxplot.pdf")
+ggsave2(mqf_fg_cell_area(movieDir, rois = ROIlist) %>% 
+          ggplot(aes(movie,area, fill=movie)) +
+          geom_boxplot() +
+          ylab("Cell area [px]")  +
+          facet_wrap(~roi) +
+          ggtitle("cell_area_boxplot"), outputFormat = "pdf")
+
+print("")
+print("Save plot: cell_area_violin.pdf")
+ggsave2(mqf_fg_cell_area(movieDir, rois = ROIlist) %>% 
+          ggplot(aes(movie,area, fill=movie)) +
+          geom_violin() +
+          ylab("Cell area [px]")  +
+          facet_wrap(~roi) +
+          ggtitle("cell_area_violin"), outputFormat = "pdf")
+## 
+
+
+
